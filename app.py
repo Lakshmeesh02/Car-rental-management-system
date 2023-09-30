3d4a7dcfdeca0b938de0c3f2f4828f42265d365b
Lakshmeesh Bhat <sherlywatson44@gmail.com>
2023-09-30 20:09:43 +0530

    db filled from user registration form but no constraints yet

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect, url_for
+import mysql.connector
 
 app=Flask(__name__)
+
+def create_sql_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="coleadonis23#J",
+        database="carrent"
+    )
 
 @app.route('/')
 def home():
@@ -12,6 +21,31 @@ def creds(login_type):
 
 @app.route('/register/<reg_type>', methods=['GET','POST'])
 def register(reg_type):
+    if request.method=="POST" and reg_type=="user":
+        username=request.form.get("cusername")
+        password=request.form.get("cpassword")
+        fname=request.form.get("fname")
+        lname=request.form.get("lname")
+        contact=request.form.get("contact")
+
+        connection=create_sql_connection()
+        cursor=connection.cursor()
+        query="insert into customer (fname, lname, username, password, contact) values (%s, %s, %s, %s, %s)"
+        data=(fname, lname, username, password, contact)
+
+        try:
+            cursor.execute(query, data)
+            connection.commit()
+            cursor.close()
+            connection.close()
+            return redirect (url_for('home'))
+        
+        except Exception as e:
+            print(e)
+            cursor.close()
+            connection.close()
+            return "Try again"
+
     return render_template(f"reg{reg_type}.html")
 
 if __name__=="__main__":
