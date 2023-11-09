ed46f2bc286a86146c85726e2de4e861c2e9c5bc
Lakshmeesh Bhat <sherlywatson44@gmail.com>
2023-11-09 09:08:47 +0530

    removed options to remove cars

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def register(reg_type):
         password=request.form.get("cpassword")
         fname=request.form.get("fname")
         lname=request.form.get("lname")
-        contact=request.form.get("contact")
+        contact=int(request.form.get("contact"))
 
         connection=create_sql_connection()
         cursor=connection.cursor()
@@ -91,7 +91,7 @@ def register(reg_type):
     if request.method=="POST" and reg_type=="company":
         password=request.form.get("password")
         name=request.form.get("name")
-        contact=request.form.get("contact")
+        contact=int(request.form.get("contact"))
         location=request.form.get("location")
 
         connection=create_sql_connection()
@@ -212,68 +212,36 @@ def user_history(customername,customer_id):
 def companypage(companyname,company_id):
     if request.method=="POST":
         carname=request.form.get("carname")
-        price_per_day=request.form.get("price_per_day")
-        action=request.form.get("action")
-        limit=request.form.get("limit")
+        price_per_day=int(request.form.get("price_per_day"))
+        limit=int(request.form.get("limit"))
 
-        if action=="add":
-            if not price_per_day or price_per_day<0:
-                return "Price per day required and needs to be valid"
-            connection=create_sql_connection()
-            cursor=connection.cursor()
-            check="select car_id, car_count, available from cars where name=%s and company_id=%s"
-            data=(carname,company_id)
-            cursor.execute(check,data)
-            existing_car=cursor.fetchone()
-            if not existing_car:
-                query="insert into cars (name,company_id,price_per_day,car_count,available) values (%s,%s,%s,%s,%s)"
-                data=(carname,company_id,price_per_day,limit,limit)
-                cursor.execute(query,data)
-                connection.commit()
-                cursor.close()
-                connection.close()
-                return "Car added successfully."
-            else:
-                car_id=existing_car[0]
-                car_count=existing_car[1]+int(limit)
-                available=existing_car[2]+int(limit)
-                query="update cars set price_per_day=%s, car_count=%s, available=%s where car_id=%s and company_id=%s"
-                data=(price_per_day,car_count,available,car_id,company_id)
-                cursor.execute(query,data)
-                connection.commit()
-                cursor.close()
-                connection.close()
-                return "updated info successfully"
-
-        elif action=="remove":
-            if not carname or not limit:
-                return "Carname and limit field necessary to perform this action"
-            connection=create_sql_connection()
-            cursor=connection.cursor()
-            check="select car_id, car_count, available from cars where name=%s and company_id=%s"
-            data=(carname,company_id)
-            cursor.execute(check,data)
-            existing_car=cursor.fetchone()
-            car_id=existing_car[0]
-            car_count=existing_car[1]-int(limit)
-            available=existing_car[2]-int(limit)
-            if car_count<0:
-                return "Invalid limit set"
-            elif car_count==0:
-                query="delete from cars where name=%s and company_id=%s"
-                data=(carname,company_id)
-                cursor.execute(query,data)
-                connection.commit()
-                cursor.close()
-                connection.close()
-                return "Car removed successfully"
-            query="update cars set car_count=%s, available=%s where car_id=%s and company_id=%s"
-            data=(car_count,available,car_id,company_id)
+        if not price_per_day or price_per_day<0:
+            return "Price per day required and needs to be valid"
+        connection=create_sql_connection()
+        cursor=connection.cursor()
+        check="select car_id, car_count, available from cars where name=%s and company_id=%s"
+        data=(carname,company_id)
+        cursor.execute(check,data)
+        existing_car=cursor.fetchone()
+        if not existing_car:
+            query="insert into cars (name,company_id,price_per_day,car_count,available) values (%s,%s,%s,%s,%s)"
+            data=(carname,company_id,price_per_day,limit,limit)
             cursor.execute(query,data)
             connection.commit()
             cursor.close()
             connection.close()
-            return "Car removed successfully"
+            return "Car added successfully."
+        else:
+            car_id=existing_car[0]
+            car_count=existing_car[1]+int(limit)
+            available=existing_car[2]+int(limit)
+            query="update cars set price_per_day=%s, car_count=%s, available=%s where car_id=%s and company_id=%s"
+            data=(price_per_day,car_count,available,car_id,company_id)
+            cursor.execute(query,data)
+            connection.commit()
+            cursor.close()
+            connection.close()
+            return "updated info successfully"
     return render_template("companyhome.html", companyname=companyname,company_id=company_id)
 
 @app.route('/company/<int:company_id>/cars',methods=['GET'])
